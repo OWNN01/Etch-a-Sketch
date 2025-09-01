28c24d852d98bb9e5935d28455ce224d0fe06742
Ownn01 <116043201+OWNN01@users.noreply.github.com>
2025-08-31 22:57:20 -0500

    Add in sizing functionality along with requried code changes to support it

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
+let height = 16;
+let width = 16;
+
 const container = document.querySelector(".container");
 
 container.style.display = "flex";
 container.style.flexDirection = "column";
 
+createGrid(width, height);
 
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.style.display = "flex";
-    gridRow.style.justifyContent = "center";
-    container.appendChild(gridRow);
-    for (let i = 0; i < 16; i++) {
-        const content = document.createElement("div");
-        content.classList.add("grid");
-        content.style.width = "10px";
-        content.style.height = "10px";
-        content.style.margin = "4px";
-        content.style.backgroundColor = "black";
-        gridRow.appendChild(content);
+function createGrid(width, height) {
+    if (width > 100 || height > 100) {
+        return;
     }
-}
+    for (let i = 0; i < height; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("gridRow");
+        gridRow.style.display = "flex";
+        gridRow.style.justifyContent = "center";
+        container.appendChild(gridRow);
+        for (let i = 0; i < width; i++) {
+            const content = document.createElement("div");
+            content.classList.add("grid");
+            content.style.width = "10px";
+            content.style.height = "10px";
+            content.style.margin = "0px";
+            content.style.backgroundColor = "black";
+            gridRow.appendChild(content);
+        }
+    }
+    let gridPoints = document.querySelectorAll(".grid");
 
-let gridPoints = document.querySelectorAll(".grid");
-
-gridPoints.forEach((e) => {
-    e.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "red";
+    gridPoints.forEach((e) => {
+        e.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "red";
+        });
     });
-});
+    const resetButton = document.querySelector(".resetButton");
+
+    resetButton.addEventListener("click", () => {
+        gridPoints.forEach((e) => e.style.backgroundColor = "black");
+    });
+};
 
 const title = document.querySelector(".title");
 title.style.display = "flex";
@@ -39,7 +53,26 @@ reset.style.display = "flex";
 reset.style.justifyContent = "center";
 reset.style.margin = "20px";
 
-reset.addEventListener("click", () => {
+const resetButton = document.querySelector(".resetButton");
+
+resetButton.addEventListener("click", () => {
     gridPoints.forEach((e) => e.style.backgroundColor = "black");
 })
+
+const sizer = document.querySelector(".sizer");
+sizer.style.display = "flex";
+sizer.style.justifyContent = "center";
+sizer.style.margin = "20px";
+
+const sizeButton = document.querySelector(".sizeButton");
+
+sizeButton.addEventListener("click", () => {
+    height = prompt("Enter a height 1-100.");
+    width = prompt("Enter a width 1-100.");
+    let divs = container.querySelectorAll(".gridRow");
+    divs.forEach((e) => container.removeChild(e));
+    createGrid(height, width);
+});
+
+
 
