24efc4454772255d8959900cd25013c2df7fa316
Ownn01 <116043201+OWNN01@users.noreply.github.com>
2025-08-31 21:56:43 -0500

    Add a title, reset button, and formatting to the preset grid

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-const container1 = document.querySelector(".container1");
-const container2 = document.querySelector(".container2");
-const container3 = document.querySelector(".container3");
-const container4 = document.querySelector(".container4");
-container1.style.display = "flex";
-container2.style.display = "flex";
-container3.style.display = "flex";
-container4.style.display = "flex";
+const container = document.querySelector(".container");
+
+container.style.display = "flex";
+container.style.flexDirection = "column";
+
 
 for (let i = 0; i < 16; i++) {
-    const content = document.createElement("div");
-    content.classList.add("grid");
-    content.style.width = "100px";
-    content.style.height = "100px";
-    content.style.margin = "16px";
-    content.style.backgroundColor = "black";
-    if (i < 4) {
-        container1.appendChild(content);
-    } else if (i < 8) {
-        container2.appendChild(content);
-    } else if (i < 12) {
-        container3.appendChild(content);
-    } else {
-        container4.appendChild(content);
+    const gridRow = document.createElement("div");
+    gridRow.style.display = "flex";
+    gridRow.style.justifyContent = "center";
+    container.appendChild(gridRow);
+    for (let i = 0; i < 16; i++) {
+        const content = document.createElement("div");
+        content.classList.add("grid");
+        content.style.width = "10px";
+        content.style.height = "10px";
+        content.style.margin = "4px";
+        content.style.backgroundColor = "black";
+        gridRow.appendChild(content);
     }
 }
 
 let gridPoints = document.querySelectorAll(".grid");
-
 
 gridPoints.forEach((e) => {
     e.addEventListener("mouseenter", (e) => {
@@ -34,4 +28,18 @@ gridPoints.forEach((e) => {
     });
 });
 
+const title = document.querySelector(".title");
+title.style.display = "flex";
+title.style.justifyContent = "center";
+title.style.margin = "20px";
+
+
+const reset = document.querySelector(".reset");
+reset.style.display = "flex";
+reset.style.justifyContent = "center";
+reset.style.margin = "20px";
+
+reset.addEventListener("click", () => {
+    gridPoints.forEach((e) => e.style.backgroundColor = "black");
+})
 
